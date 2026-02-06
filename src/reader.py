33d835dfd83757ff1a6596d89b4b1d2c7c181bb9
Daniil Tarasov <danya.tarasov.01@mail.ru>
2026-02-06 22:16:16 +0300

    fxs reader

--- src/reader.py
+++ src/reader.py
@@ -1,28 +1,34 @@
 import csv
 
 
-def get_data_from_csv(path_to_the_file: str):
-    """Функция, которая возвращает данные из файла csv"""
-    country_gdps = {}
-    try:
-        with open(path_to_the_file, encoding="utf-8") as file:
-            csv.DictReader(file)
+def get_data_from_csv(path_to_file):
+    """Функция, которая возвращает данные (country, gdp) из файла csv"""
+    all_country_gdps = {}
 
-    except ValueError:
-
-        return []
-
-    except FileNotFoundError:
-
-        return []
-
+    if isinstance(path_to_file, str):
+        files = [path_to_file]
     else:
-        with open(path_to_the_file, encoding="utf-8") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                country = row["country"]
-                gdp = row["gdp"]
-                if country not in country_gdps:
-                    country_gdps[country] = []
-                country_gdps[country].append(gdp)
-        return country_gdps
+        files = path_to_file
+
+    for file_path in files:
+        country_gdps = {}
+        try:
+            with open(file_path, encoding="utf-8") as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    country = row["country"]
+                    gdp = int(row["gdp"])
+                    if country not in country_gdps:
+                        country_gdps[country] = []
+                    country_gdps[country].append(gdp)
+
+                for country, gdps in country_gdps.items():
+                    if country not in all_country_gdps:
+                        all_country_gdps[country] = []
+                    all_country_gdps[country].extend(gdps)
+
+        except FileNotFoundError:
+            print(f"Файл не найден: {file_path}")
+            continue
+
+    return all_country_gdps
